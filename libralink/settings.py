f7839b31e1f05842c970e3cf6b0c42e62e9ebd83
Riza May Largadas <rmlargadas@gmail.com>
2024-05-10 17:35:40 +0800

    remove / in CSRF_TRUSTED_ORIGINS

--- libralink/settings.py
+++ libralink/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = False
 
 ALLOWED_HOSTS = ['web-production-d25b.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-d25b.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-d25b.up.railway.app']
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
