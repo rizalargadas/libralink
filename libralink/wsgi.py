762ba9207ddf06096d3d9403bfc86da58e05094d
Riza May Largadas <rmlargadas@gmail.com>
2024-05-10 17:50:56 +0800

    delete STATICFILESDIRS, added whitenoise configs on wsgi.py

--- libralink/wsgi.py
+++ libralink/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
+from whitenoise import WhiteNoise
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'libralink.settings')
 
 application = get_wsgi_application()
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+
+application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), 'media'), prefix='/media/')
